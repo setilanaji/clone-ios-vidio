0444d8a5cb07f70af86b5b974de921ae915a296a
Yudha <setyajiyudha299@gmail.com>
2022-03-31 18:17:39 +0700

    Initial Commit

---
+++ vidio clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vidio clone
+//
+//  Created by Yudha Setyaji on 2022/3/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
